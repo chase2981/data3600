29b59fee4328f0ba46a77b8744c9a8633fc91f5d
Ubuntu <ubuntu@ip-172-31-17-198.us-west-1.compute.internal>
2023-10-24 22:14:12 +0000

    add db

---
+++ config.py
@@ -0,0 +1,10 @@
+import pymysql
+
+USERNAME = 'admin'
+PASSWORD = ''
+ENDPOINT = 'database-1.cjaswyat8bsq.us-west-1.rds.amazonaws.com'
+PORT = 3306
+REGION = 'us-west-1c'
+SSL_CA = 'ssl/rds-combined-ca-bundle.pem'
+CURSORCLASS = pymysql.cursors.DictCursor
+DBNAME = 'tempdatabase'
